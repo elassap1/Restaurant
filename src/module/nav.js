7fa39f4a7989889e656587410beae176d01ec7b2
irassari <elassari19@gmail.com>
2021-01-20 16:17:16 +0100

    delete

--- src/module/nav.js
+++
@@ -1,7 +0,0 @@
-import create from './functions'
-
-create('div', 1, '', "",'id', "nav", "#content");
-create('span', 1, 'ROBALO', "",'', "", "#nav");
-create('a', 1, 'Home', "home",'href', "#home", "#nav");
-create('a', 1, 'Menu', "menu",'href', "#menu", "#nav");
-create('a', 1, 'Contact', "contact",'href', "#contact", "#nav");
